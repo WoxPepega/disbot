acd3bb9e2ec8a5072f081678642f1a837677493d
WoxPepega <62587395+WoxPepega@users.noreply.github.com>
2021-01-28 19:42:49 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+	'token': 'Mjc0NTYwNTI5NjIyODkyNTQ0.X42VIw.LZZeMzbBQmO_PWA50xxfejE8WSA',
+	'bot': 'Имя бота',
+	'id': 274560529622892544,
+	'prefix': '>>',
+}
+
+print(settings['prefix'])
